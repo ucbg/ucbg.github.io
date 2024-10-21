be7a80d6c9ef554ee56934f33ee23b4770d9c003
ucbg <hamidogs22@gmail.com>
2024-10-21 10:15:45 +0200

    66

--- assets/js/analytics_ucbg_v1.js
+++ assets/js/analytics_ucbg_v1.js
@@ -1,24 +1,23 @@
 function loadGoogleAnalytics(id) {
-    // Google tag (gtag.js)
-    var firstScript= document.getElementsByTagName("script")[0];
-    newScript= document.createElement("script");
-    newScript.async= "";
-    newScript.src= "https://www.googletagmanager.com/gtag/js?id="+ id;
-    firstScript.parentNode.insertBefore(newScript, firstScript);
+  // Google tag (gtag.js)
+  var firstScript = document.getElementsByTagName("script")[0];
+  let newScript = document.createElement("script");
+  newScript.async = true;
+  newScript.src = "https://www.googletagmanager.com/gtag/js?id=" + id;
+  firstScript.parentNode.insertBefore(newScript, firstScript);
 
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-    gtag('config', id);
+  window.dataLayer = window.dataLayer || [];
+  function gtag() { dataLayer.push(arguments); }
+  gtag('js', new Date());
+  gtag('config', id);
 }
 
 window.addEventListener("load", function() {
-    if (navigator.webdriver) {
+  if (navigator.webdriver) {
       console.log('Bot Browser');
       loadGoogleAnalytics("G-T445XL67R6");
-
-    } else {
+  } else {
       console.log('Human Browser');
       loadGoogleAnalytics("G-T445XL67R6");
-    }
+  }
 });
