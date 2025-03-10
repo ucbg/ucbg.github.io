113269c2145fdcc717c57d67df0859eb77004637
Hamdi YILMAZ <hamidogs22@gmail.com>
2025-03-11 00:20:27 +0100

    5

--- assets/js/load-games.js
+++ assets/js/load-games.js
@@ -7,36 +7,58 @@ document.addEventListener("DOMContentLoaded", async () => {
     // Kartların ekleneceği container elementi
     const cardContainer = document.querySelector(".card-masonry");
 
-    // Her 10-15 kart arasında bir large kart olacak şekilde rastgele sayı üret
-    const randomInterval = Math.floor(Math.random() * (15 - 10 + 1)) + 10;
-
     // Kartları oluştur
     games.forEach((game, index) => {
-      // Her randomInterval karttan birini large yap
+      // 20'de bir reklam kartı ekle
+      if ((index + 1) % 20 === 0) {
+        const adHTML = `
+          <a class="card large">
+            <script
+              async
+              src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7321073664976914"
+              crossorigin="anonymous"
+            ></script>
+            <!-- oyun aralari -->
+            <ins
+              class="adsbygoogle"
+              style="display: inline-block; width: 260px; height: 260px"
+              data-ad-client="ca-pub-7321073664976914"
+              data-ad-slot="1811365994"
+            ></ins>
+            <script>
+              (adsbygoogle = window.adsbygoogle || []).push({});
+            </script>
+          </a>
+        `;
+        cardContainer.insertAdjacentHTML("beforeend", adHTML);
+      }
+
+      // Her 10-15 kart arasında bir large kart olacak şekilde rastgele sayı üret
+      const randomInterval = Math.floor(Math.random() * (15 - 10 + 1)) + 10;
       const isLarge = index % randomInterval === 0;
 
       const cardHTML = `
-                <a href="${game.url}" class="card${isLarge ? " large" : ""}">
-                    <picture>
-                        <source
-                            data-srcset="${game.image}"
-                            type="image/png"
-                            class="img-fluid"
-                            srcset="${game.image}"
-                        />
-                        <img
-                            data-src="${game.image}"
-                            alt="${game.title}"
-                            class="lazyload img-fluid"
-                            width="500"
-                            height="500"
-                        />
-                    </picture>
-                    <div class="card-body">
-                        <h3>${game.title}</h3>
-                    </div>
-                </a>
-            `;
+        <a href="${game.url}" class="card${isLarge ? " large" : ""}">
+            <picture>
+                <source
+                    data-srcset="${game.image}"
+                    type="image/png"
+                    class="img-fluid"
+                    srcset="${game.image}"
+                />
+                <img
+                    data-src="${game.image}"
+                    alt="${game.title}"
+                    class="lazyload img-fluid"
+                    width="500"
+                    height="500"
+                />
+            </picture>
+            <div class="card-body">
+                <h3>${game.title}</h3>
+            </div>
+        </a>
+      `;
 
       // innerHTML kullanarak HTML'i ekle
       cardContainer.insertAdjacentHTML("beforeend", cardHTML);
