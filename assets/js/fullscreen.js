5f44f0f2d77f7a23fe9159607f594b1c083d4398
Hamdi YILMAZ <hamidogs22@gmail.com>
2025-03-24 18:28:54 +0100

    yeni oyunlar eklendi

--- assets/js/fullscreen.js
+++ assets/js/fullscreen.js
@@ -20,8 +20,8 @@ const server1BaseUrl = urlParts.slice(0, 3).join("/");
 
 // Dinamik olarak eklenecek serverlar
 const extraServers = [
-  { name: "2", baseUrl: "https://unblockedgames66.gitlab.io" },
-  { name: "3", baseUrl: "https://unblockedgames67.gitlab.io" },
+  { name: "2", baseUrl: "https://unblockedgames67.gitlab.io" },
+  { name: "3", baseUrl: "https://unblockedgames66.gitlab.io" },
   { name: "4", baseUrl: "https://ubgwtf.gitlab.io" },
 ];
 
