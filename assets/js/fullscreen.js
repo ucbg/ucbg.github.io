848a734501a7974905b829fc36ad4a9a60c5c161
Hamdi YILMAZ <hamidogs22@gmail.com>
2025-03-24 17:23:14 +0100

    8

--- assets/js/fullscreen.js
+++ assets/js/fullscreen.js
@@ -22,7 +22,7 @@ const server1BaseUrl = urlParts.slice(0, 3).join("/");
 const extraServers = [
   { name: "2", baseUrl: "https://unblockedgames66.gitlab.io" },
   { name: "3", baseUrl: "https://unblockedgames67.gitlab.io" },
-  { name: "3", baseUrl: "https://ubgwtf.gitlab.io" },
+  { name: "4", baseUrl: "https://ubgwtf.gitlab.io" },
 ];
 
 // Sunucu listesini oluştur
