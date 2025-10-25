323631161913d98deec77126908750946ab0ee51
Hamdi YILMAZ <hamdiyilmaz85@gmail.com>
2025-10-26 00:08:07 +0200

    32

--- assets/js/recent_games.js
+++ assets/js/recent_games.js
@@ -92,7 +92,7 @@ async function showRecentGames() {
   }
 
   try {
-    let response = await fetch("/data-json/games.json?v=2.0.2");
+    let response = await fetch("/data-json/games.json?v=2.0.3");
     let gamesData = await response.json();
 
     let cardsContainer = document.createElement("div");
