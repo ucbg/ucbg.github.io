7beb7d578f275616196bd9cb9f8ef9603a55cafe
Hamdi YILMAZ <hamdiyilmaz85@gmail.com>
2025-10-28 20:07:51 +0100

    32

--- assets/js/populate-games.js
+++ assets/js/populate-games.js
@@ -30,7 +30,7 @@ document.addEventListener("DOMContentLoaded", async () => {
 
   try {
     // Fetch games data
-    const response = await fetch("/data-json/games.json?v=2.0.23");
+    const response = await fetch("/data-json/games.json?v=2.0.24");
 
     if (!response.ok) {
       throw new Error(`HTTP error! status: ${response.status}`);
