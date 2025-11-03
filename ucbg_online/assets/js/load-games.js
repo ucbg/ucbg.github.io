a6f59ac6dd5238caaa9c072d390432e7e0f7ac93
Hamdi YILMAZ <hamdiyilmaz85@gmail.com>
2025-11-03 15:21:43 +0100

    32

--- ucbg_online/assets/js/load-games.js
+++ ucbg_online/assets/js/load-games.js
@@ -61,7 +61,7 @@ document.addEventListener("DOMContentLoaded", async () => {
 
     await loadGammeData();
 
-    const response = await fetch("/data-json/games.json?v=2.0.45");
+    const response = await fetch("/data-json/games.json?v=2.0.46");
     const allGames = await response.json();
 
     const games = filters
@@ -152,7 +152,7 @@ document.addEventListener("DOMContentLoaded", async () => {
       }
     }
 
-    const response = await fetch("/data-json/games.json?v=2.0.45");
+    const response = await fetch("/data-json/games.json?v=2.0.46");
     const games = await response.json();
 
     function getRandomGames(games, count) {
