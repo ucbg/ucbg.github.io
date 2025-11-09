7e9261e969cce6bfe05ce7f8f27d18cd58bdee13
Hamdi YILMAZ <hamdiyilmaz85@gmail.com>
2025-11-09 05:26:18 +0100

    32

--- assets/js/carrousel_featured.js
+++ assets/js/carrousel_featured.js
@@ -51,7 +51,7 @@ document.addEventListener("DOMContentLoaded", async () => {
 
     await loadGammeData();
 
-    const response = await fetch("/data-json/games.json?v=2.0.61");
+    const response = await fetch("/data-json/games.json?v=2.0.62");
     const allGames = await response.json();
 
     // NEW GAMES: Son 20 oyunu al ve tersine çevir
--- assets/js/recent_games.js
+++ assets/js/recent_games.js
@@ -92,7 +92,7 @@ async function showRecentGames() {
   }
 
   try {
-    let response = await fetch("/data-json/games.json?v=2.0.61");
+    let response = await fetch("/data-json/games.json?v=2.0.62");
     let gamesData = await response.json();
 
     let cardsContainer = document.createElement("div");
