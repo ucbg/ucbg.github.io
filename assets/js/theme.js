17f45c18a8464dbd42fdd6ad87401c36eff3b345
Hamdi YILMAZ <hamdiyilmaz85@gmail.com>
2025-12-09 01:15:35 +0100

    22

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,7 +1,1 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const htmlElement = document.documentElement;
-
-  // Always set theme to dark
-  htmlElement.setAttribute("data-theme", "dark");
-  localStorage.setItem("theme", "dark");
-});
+document.addEventListener("DOMContentLoaded",()=>{document.documentElement.setAttribute("data-theme","dark"),localStorage.setItem("theme","dark")});
